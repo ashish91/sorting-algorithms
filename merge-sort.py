f9a8600ad919db83cba78e5ce063b7835ca6d9e6
Ashish Gaur <ashishgaurp91@gmail.com>
2022-10-04 19:40:57 +0530

    Sorting revisited

--- merge-sort.py
+++ merge-sort.py
@@ -1,33 +1,36 @@
-def mergeSort(arr):
-	N = len(arr)
-	if N <= 1:
-		return
+def merge(arr, low, mid, high):
+  N = mid + 1 - low
+  M = high - mid
 
-	mid = N//2
-	L = arr[:mid]
-	R = arr[mid:]
+  aux = [None]*(N+M)
 
-	mergeSort(arr)
-	mergeSort(arr)
+  i, j, k = low, mid+1, 0
+  while i < N and j < M:
+    if arr[i] <= arr[j]:
+      aux[k] = arr[i]
+      i += 1
+    else:
+      aux[k] = arr[j]
+      j += 1
+    k += 1
 
-	i = j = k = 0
+  while i < N:
+    aux[k] = arr[i]
+    i += 1
+    k += 1
 
-	while i < len(L) and j < len(R):
-		if L[i] < R[j]:
-			arr[k] = L[i]
-			i += 1
-		else:
-			arr[k] = L[j]
-			j += 1
+  while j < M:
+    aux[k] = arr[j]
+    j += 1
+    k += 1
 
-		k += 1
+  # Copy elements back to arr from aux
+  for i in range(N+M):
+    arr[i+low] = aux[i]
 
-	while i < len(L):
-		arr[k] = L[i]
-		i += 1
-		k += 1
-
-	while j < len(R):
-		arr[k] = R[j]
-		j += 1
-		k += 1
+def mergesort(arr, s, e):
+  if s < e:
+    mid = s + (e-s)//2
+    mergesort(arr, s, mid)
+    mergesort(arr, mid+1, e)
+    merge(arr, s, mid, e)
---
+++ quick-sort.py
@@ -0,0 +1,22 @@
+def partition(arr, l, h):
+  p = s
+
+  while l < h:
+    while l <= h and arr[l] <= arr[p]:
+      l += 1
+
+    while l <= h and arr[p] < arr[h]:
+      h -= 1
+
+    if l < h:
+      arr[l], arr[h] = arr[h], arr[l]
+
+  arr[p], arr[h] = arr[h], arr[p]
+  return h
+
+
+def quicksort(arr, s, e):
+  if s < e:
+    mid = partition(arr, s, e)
+    quicksort(arr, s, mid-1)
+    quicksort(arr, mid+1, e)
